42ad689f3bb5885e349dcfb1d35597113553a782
Arthur-king18 <arturik060406@gmail.com>
2024-01-29 20:28:27 +0400

    bug fix

--- backend/src/project/telegram/keyboards/inline/delete.py
+++ backend/src/project/telegram/keyboards/inline/delete.py
@@ -1,7 +1,7 @@
 from aiogram import types
 from aiogram.utils.i18n import gettext
 
-from project.telegram.events.delete import DeleteEvent
+from backend.src.project.telegram.events.delete import DeleteEvent
 
 
 def create_delete_button() -> types.InlineKeyboardButton:
